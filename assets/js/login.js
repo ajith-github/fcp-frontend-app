db2ebb3861f0a559c38c94594ec8cabbf8e5a54e
ajith <ajith.thomas@freshworks.com>
2019-11-13 00:56:46 +0530

    login api.

---
+++ assets/js/login.js
@@ -0,0 +1,44 @@
+'use strict';
+var BASE_API_URL = 'http://localhost:3000';
+
+function getGoogleSSO(){
+    try {
+        let API_URL = BASE_API_URL + '/google/login'
+        axios.get(API_URL, { crossdomain: true })
+        .then(function (response) {
+            console.log('[getGoogleSSO] response: ', response);
+            $("#google-sso-link").attr("href",response.data.google_url);
+        })
+        .catch(function (error) {
+            console.log('[getGoogleSSO] error: ', error);
+        })
+
+    } catch (error) {
+        console.log('[getGoogleSSO] error: ', error)
+    }
+}
+
+
+function signIn(){
+    try {
+        let API_URL = BASE_API_URL + '/login'
+        let email = $('#user-email').val()
+        let passwd = $('#user-passwd').val()
+        console.log('email: %s, password: %s', email, passwd)
+        axios.post(API_URL, {
+            email: email,
+            password: passwd
+        })
+        .then(function (response) {
+            console.log('[signIn] axios response: ', response);
+            window.location.href = '/'
+        })
+        .catch(function (error) {
+            console.log('[signIn] axios error: ', error);
+        });
+    } catch (error) {
+        console.log('[signIn] err: ', err)
+    }
+
+
+}
