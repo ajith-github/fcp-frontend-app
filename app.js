f7b683212f4386dc747c61c9aed60bc1b12db790
ajith <ajith.thomas@freshworks.com>
2019-11-15 12:17:30 +0530

    frontend app google auth completed.

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const session = require('express-session');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var uploadRouter = require('./routes/upload');
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,10 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/upload', uploadRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
+const axios = require('axios')
 let google_auth = require('../lib/google-auth')
-let mysql = require('../lib/mysql')
-const APP_URL = process.env.APP_URL
+
+let BASE_API_URL = process.env.BASE_API_URL
 
 let homepage = function(req, res){
+    console.log('homepage')
     let user_details = false
     if (req.session.hasOwnProperty('user')){
         user_details = req.session.user
@@ -31,10 +33,24 @@ let loginpage = function(req, res) {
 let loginHandler = async function(req, res) {
     let params = req.body
     console.log('params: ', params)
-    db = await mysql.connect()
-    return res.status(200).send('OK')
-}
 
+    let API_URL = BASE_API_URL + '/login'
+    console.log('API_URL: ', API_URL)
+
+    axios.post(API_URL, {
+        email: params.email,
+        password: params.password
+    })
+    .then(function (response) {
+        console.log('[signIn] axios response: ', response);
+        res.render('pages/index')
+    })
+    .catch(function (error) {
+        console.log('error: ', error)
+        res.render('pages/login')
+    });
+
+}
 
 let logoutHandler = async function(req, res) {
     let logged_out = await req.session.destroy()
@@ -51,13 +67,13 @@ let callbackHandler = async function(req, res) {
     console.log('code: ', code)
     let user_details = await google_auth.getGoogleAccountFromCode(code)
     console.log('user_details: ', user_details)
+
     if (user_details.hasOwnProperty('id')){
         req.session.user = user_details
-        // res.status(200).json({...user_details})
-        res.status(301).redirect(APP_URL + '/index.html')
+        res.redirect('/')
         return
     } else {
-        res.redirect(APP_URL + '/login')
+        res.redirect('pages/login')
     }
 }
 
